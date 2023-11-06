2e722430a93ef7269e996299b75a8bd2fecfcb2b
jules <142106821+Jules-12-8@users.noreply.github.com>
2023-11-06 14:50:58 +0100

    refactor: simplify playRound function, remove the two precedent functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,42 +1,51 @@
-let userSelection = "scissors";
-let computerSelection = "rock";
+let playerSelection = "paper";
+let computerSelection = "scissors";
 let userScore = 0;
 let computerScore = 0;
 
-//In the getRoundResult function the two cells array permit to reversely test the conditionals with a recursive call and make the function shorter.
-function getRoundResult(user, computer) {
-    //!Ajouter toLowerCase
-    let result = [];
-    if (user === "rock" && computer === "paper") {
-        result = [0, 1];
-    } else if (user === "rock" && computer === "scissors") {
-        result = [1, 0];
-    } else if (user === "paper" && computer === "scissors") {
-        result = [0, 1];
-    } else if (user === computer) {
-        result = [0, 0];
-    } else {
-        result = getRoundResult(computer, user);
-        result = result.reverse();
+
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case computerSelection.toLowerCase():
+            console.log("It's a draw!");
+            break;
+        case "rock":
+            if (computerSelection.toLowerCase() === "paper") {
+                console.log("You loose this round!");
+                return "computer";
+            } else if (computerSelection.toLowerCase() === "scissors") {
+                console.log("You won this round!");
+                return "player";
+            } else {
+                console.error("There is an error");
+            }
+            break;
+        case "paper":
+            if (computerSelection.toLowerCase() === "rock") {
+                console.log("You won this round!");
+                return "player";
+            } else if (computerSelection.toLowerCase() === "scissors") {
+                console.log("You loose this round!");
+                return "computer";
+            } else {
+                console.error("There is an error");
+            }
+            break;
+        case "scissors":
+            if (computerSelection.toLowerCase() === "rock") {
+                console.log("You loose this round!");
+                return "computer";
+            } else if (computerSelection.toLowerCase() === "paper") {
+                console.log("You won this round!");
+                return "player";
+            } else {
+                console.error("There is an error");
+            }
+            break;
+        default:
+            console.error("There is an error");
     }
-    return result
 }
 
-let roundResult = getRoundResult(userSelection, computerSelection);
-
-function setRoundScore(roundResult) {
-    if (roundResult[0] === 0 && roundResult[1] === 0) {
-        console.log(`It's a draw! ${userScore}`);
-    } else if (roundResult[0] === 1) {
-        userScore = userScore + 1;
-        console.log(`You won this round! ${userScore}`);
-    } else if (roundResult[1] === 1) {
-        computerScore = computerScore + 1;
-        console.log(`You loose this round ${computerScore}`);
-    }
-}
-
-
-
-console.log(roundResult);
-console.log(setRoundScore(roundResult));
+console.log(playRound(playerSelection, computerSelection));
